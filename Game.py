b926ccdb31d8640920e0111f62a1ac67d8a2ee3c
artem <artos2200@gmail.com>
2020-11-16 22:18:23 +0200

    move player metods to Player class

--- Game.py
+++ Game.py
@@ -6,39 +6,14 @@ from Player import Player
 class Game:
     def __init__(self, board: Board, players: [Player]):
         self.zero_array: np.array = np.zeros((board.size, board.size), dtype=int)
-        self.board = board
-        self.players = players
+        self.board: Board = board
+        self.players: [Player] = players
 
-    def player_field_get(self, player_id):
-        return np.where(self.board.field == player_id, self.board.field, self.zero_array) // player_id
-
-    def move(self, player_id, x, y):
+    def move(self, player: Player, x: int, y: int):
         if self.board.field[x][y] == 0:
-            self.board.field[x][y] = player_id
-            if self.has_5in_row(player_id):
+            self.board.player_id_cell_set(x, y, player.player_id)
+            player.field_set(self.board.player_field_get(player.player_id))
+            has5_in_row = player.has_5in_row()
+            if has5_in_row:
                 return 'WIN'
 
-    def has_5in_row(self, player_id):
-        target = 2
-        player_field = self.player_field_get(player_id)
-        x = (np.sum(player_field, axis=0))
-        for i in x:
-            if (np.sum(i)) >= target:
-                return True
-
-        y = (np.sum(player_field, axis=1))
-        for i in y:
-            if (np.sum(i)) >= target:
-                return True
-
-        for i in range(self.board.size):
-            # diagonal sum
-            if (np.sum(player_field.diagonal(offset=i, axis1=0, axis2=1))) >= target:
-                return True
-            if (np.sum(player_field.diagonal(offset=i+1, axis1=1, axis2=0))) >= target:
-                return True
-            # fliplr diagonal sum
-            if (np.sum(np.fliplr(player_field).diagonal(offset=i, axis1=0, axis2=1))) >= target:
-                return True
-            if (np.sum(np.fliplr(player_field).diagonal(offset=i+1, axis1=1, axis2=0))) >= target:
-                return True
