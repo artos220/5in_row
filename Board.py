7e6ffb6742bc85ce1430dd02e82d62fbfeb81ea4
artem <artos2200@gmail.com>
2021-01-09 10:31:15 +0200

    prettify

--- Board.py
+++ Board.py
@@ -3,16 +3,17 @@ import numpy as np
 
 class Board:
     def __init__(self, size: int):
-        self.zero_array: np.array = np.zeros((size, size), dtype=int)
+        self.ZERO_ARRAY_: np.array = np.zeros((size, size), dtype=int)
         self.size: int = size
-        self.field: np.array = self.zero_array.copy()
+        self.field: np.array = self.ZERO_ARRAY_.copy()
 
-    def player_id_cell_set(self, x: int, y: int, player_id: int):
-        self.field[x][y] = player_id
+    def cell_set(self, y: int, x: int, player_id: int):
+        self.field[y][x] = player_id
 
     def player_field_get(self, player_id: int) -> np.array:
-        return np.where(self.field == player_id, self.field, self.zero_array) // player_id
+        return np.where(self.field == player_id, self.field, self.ZERO_ARRAY_.copy()).copy() // player_id
 
     def show(self):
+        print('--')
         for i in self.field:
             print(i)
