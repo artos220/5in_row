d02301cc71076f0531dd33e372c680648413fc92
artem <artos2200@gmail.com>
2021-01-09 10:32:33 +0200

    move methods to Game

--- Game.py
+++ Game.py
@@ -1,19 +1,61 @@
-import numpy as np
 from Board import Board
 from Player import Player
 
 
+def input_(msg):
+    while True:
+        try:
+            y, x = input(msg).strip().split()
+            if not y.isdigit() or not x.isdigit():
+                print('Need two positive numbers like: 0 0')
+                continue
+        except ValueError as e:
+            print('ValueError: not enough values to unpack')
+            continue
+        return y, x
+
+
 class Game:
     def __init__(self, board: Board, players: [Player]):
-        self.zero_array: np.array = np.zeros((board.size, board.size), dtype=int)
         self.board: Board = board
         self.players: [Player] = players
 
-    def move(self, player: Player, x: int, y: int):
-        if self.board.field[x][y] == 0:
-            self.board.player_id_cell_set(x, y, player.player_id)
-            player.field_set(self.board.player_field_get(player.player_id))
-            has5_in_row = player.has_5in_row()
-            if has5_in_row:
-                return 'WIN'
+    def start(self):
+        self.board.show()
+        game_over = False
+        while not game_over:
+            for player in self.players:
+                print(player.name + ' move')
+                game_over = self.make_iteration(player)
+                self.board.show()
 
+                if game_over:
+                    break
+
+    # on each iteration player try's to make a move
+    def make_iteration(self, player: Player):
+        while True:
+            y, x = input_('input coordinates row col:')
+            status = 'REPEAT'
+            try:
+                status = self.move(player, int(y), int(x))
+            except (IndexError, ValueError):
+                print(f'Out of board size, current is {self.board.size}')
+
+            if status == 'WIN':
+                print(player.name + ' WIN!')
+                return True
+            elif status == 'REPEAT':
+                continue
+            return False
+
+    # player make move by setting his player_id in board.field[y][x]
+    def move(self, player: Player, y: int, x: int):
+        if self.board.field[y][x] != 0:
+            print('Cell in use')
+            return 'REPEAT'
+
+        self.board.cell_set(y, x, player.player_id)
+        if player.has_won(y, x):
+            return 'WIN'
+        return 'OK'
--- main.py
+++ main.py
@@ -1,36 +1,11 @@
-import numpy as np
 from Game import Game
 from Board import Board
 from Player import Player
 
-size = int(input('set field:'))
-playfield = Board(size=size)
-playfield.show()
+size = int(input('set field:'))  # board size
+board = Board(size=size)
 
-player1 = Player(player_id=1, name='Bob')
-player2 = Player(player_id=2, name='Sara')
-game = Game(playfield, [player1, player2])
-
-
-def iteration(player: Player):
-    playfield.show()
-
-    while True:
-        x, y = input('input coordinates x y:').strip().split()
-        status = game.move(player, int(x), int(y))
-        if status == 'WIN':
-            print(player.name + ' WIN!')
-            return False
-        return True
-
-
-repeat = True
-while repeat:
-    print(player1.name + ' move')
-    repeat = iteration(player1)
-
-    if not repeat:
-        break
-
-    print(player2.name + ' move')
-    repeat = iteration(player2)
+player1 = Player(player_id=1, name='Bob', size=size)
+player2 = Player(player_id=2, name='Sara', size=size)
+game = Game(board, [player1, player2])
+game.start()
