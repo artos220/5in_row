819c4996cef1baab3c07cebcff17ff09f66ad92d
artem <artos2200@gmail.com>
2021-01-10 17:03:55 +0200

    add some helper staff

--- Game.py
+++ Game.py
@@ -19,23 +19,39 @@ class Game:
     def __init__(self, board: Board, players: [Player]):
         self.board: Board = board
         self.players: [Player] = players
+        self.current_player: Player = players[0]
+        self.game_over: bool = False
+
+    def switch_player(self):
+        curr_player_id = self.current_player.player_id
+        if curr_player_id < len(self.players):
+            self.current_player = self.players[curr_player_id]
+        else:
+            self.current_player = self.players[0]
+        return curr_player_id
+
+    def restart(self):
+        self.board.clear()
+        self.current_player = self.players[0]
+        self.game_over = False
+        for player in self.players:
+            player.clear()
 
     def start(self):
         self.board.show()
-        game_over = False
-        while not game_over:
+        while not self.game_over:
             for player in self.players:
                 print(player.name + ' move')
-                game_over = self.make_iteration(player)
+                self.game_over = self.make_iteration(player)
                 self.board.show()
 
-                if game_over:
+                if self.game_over:
                     break
 
     # on each iteration player try's to make a move
-    def make_iteration(self, player: Player):
+    def make_iteration(self, player: Player, x: int, y: int):
         while True:
-            y, x = input_('input coordinates row col:')
+            # y, x = input_('input coordinates row col:')
             status = 'REPEAT'
             try:
                 status = self.move(player, int(y), int(x))
@@ -44,16 +60,24 @@ class Game:
 
             if status == 'WIN':
                 print(player.name + ' WIN!')
+                self.game_over = True
                 return True
             elif status == 'REPEAT':
                 continue
+            elif status == 'PASS':
+                pass
+            elif status == 'BREAK':
+                break
+
+            self.switch_player()
             return False
 
     # player make move by setting his player_id in board.field[y][x]
     def move(self, player: Player, y: int, x: int):
         if self.board.field[y][x] != 0:
             print('Cell in use')
-            return 'REPEAT'
+            # return 'REPEAT'
+            return 'BREAK'
 
         self.board.cell_set(y, x, player.player_id)
         if player.has_won(y, x):
